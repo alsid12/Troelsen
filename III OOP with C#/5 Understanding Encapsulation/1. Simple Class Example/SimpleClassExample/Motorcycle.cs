04c346ddf83000ecdc430e399240879397279844
Aliaksandr_Sidarau <aliaksandr_sidarau@epam.com>
2018-11-08 18:54:33 +0300

    Chapter 5, encapsulation.

---
+++ III OOP with C#/5 Understanding Encapsulation/1. Simple Class Example/SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+        public string name;
+
+        // Constructor chaining.
+        public Motorcycle()
+        {
+            Console.WriteLine("In default constructor");
+        }
+
+        public Motorcycle(int intensity)
+            : this(intensity, "")
+        {
+            Console.WriteLine("In ctor taking an int");
+        }
+
+        public Motorcycle(string name) : this(0, name)
+        {
+            Console.WriteLine("In ctor taking a string");
+        }
+
+        // This is the master constructor making the real work.
+        public Motorcycle(int intensity, string name)
+        {
+            Console.WriteLine("In master constructor");
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+
+            driverIntensity = intensity;
+            this.name = name;
+        }
+
+        public void SetDriverName(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+}
---
+++ III OOP with C#/5 Understanding Encapsulation/1. Simple Class Example/SimpleClassExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+
+            // Make a motorcycle.
+            Motorcycle c = new Motorcycle(5, "Fred");
+            c.SetDriverName("Billy");
+
+            Console.WriteLine("Rider name is {0}", c.name);
+            Console.ReadLine();
+        }
+    }
+}
