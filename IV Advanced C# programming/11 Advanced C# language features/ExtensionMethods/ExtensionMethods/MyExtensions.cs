ee7d2191aa6e8f9a1986a6a06edd7053c4e920f1
aliaksandr_sidarau <alexander.g.sidorov@gmail.com>
2018-12-25 23:46:52 +0300

    Modified MyExtensions project.

--- IV Advanced C# programming/11 Advanced C# language features/ExtensionMethods/ExtensionMethods/MyExtensions.cs
+++ IV Advanced C# programming/11 Advanced C# language features/ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ExtensionMethods
 {
-    class MyExtensions
+    static class MyExtensions
     {
+        // This method allows any object to display the assembly it is defined in.
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine($"{obj.GetType().Name} lives here: => {Assembly.GetAssembly(obj.GetType()).GetName().Name}\n");
+        }
+
+        // This method allows any integer to reverse its digits.
+        // For example, 56 would return 65.
+        public static int ReverseDigits(this int i)
+        {
+            // Translate int to a string and then ge tall the characters.
+            char[] digits = i.ToString().ToCharArray();
+
+            // Now, reverse items in the resulting array.
+            Array.Reverse(digits);
+
+            // Put back to a string.
+            string newDigits = new string(digits);
+
+            // Finally, return the modified string back as an int.
+            return int.Parse(newDigits);
+        }
     }
 }
--- IV Advanced C# programming/11 Advanced C# language features/ExtensionMethods/ExtensionMethods/Program.cs
+++ IV Advanced C# programming/11 Advanced C# language features/ExtensionMethods/ExtensionMethods/Program.cs
@@ -10,6 +10,24 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with extension methods *****\n");
+
+            // This int has assumed a new identity!
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            // So has the data set!
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            // And the soundplayer!
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            // Use new integer functionality.
+            Console.WriteLine("Value of myInt: {0}", myInt.ReverseDigits());
+
+            Console.ReadLine();
         }
     }
 }
