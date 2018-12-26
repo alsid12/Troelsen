ad6556e170fb2e26985e1d8061dd76ef6f835a93
Aliaksandr_Sidarau <aliaksandr_sidarau@epam.com>
2018-12-26 18:25:48 +0300

    Extension of interfaces test.

---
+++ IV Advanced C# programming/11 Advanced C# language features/InterfaceExtensions/InterfaceExtensions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Extending Interface Compatible Types *****\n");
+            // System.Array implements IEnumerable!
+            string[] data = {"Wow", "this", "is", "sort", "of", "annoying", "but", "in", "a", "weird", "way", "fun!"};
+            data.PrintDataAndBeep();
+
+            Console.WriteLine();
+
+            // List<> implements IEnumerable!
+            List<int> myInts = new List<int>() {10, 15, 20};
+            myInts.PrintDataAndBeep();
+            Console.ReadLine();
+        }
+    }
+}
