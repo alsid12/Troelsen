8e1701b9d11b0c9a54b16b206fee2204cdee15b1
Aliaksandr_Sidarau <aliaksandr_sidarau@epam.com>
2019-01-10 00:25:58 +0300

    Simple custom exception project

---
+++ III OOP with C#/7 Understanding Structured Exception Handling/1 SimpleException/1 SimpleException/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple exception example *****\n");
+            Console.WriteLine("=> creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 35);
+            myCar.CrankTunes(true);
+
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    myCar.Accelerate(10);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n *** Error! ***");
+                Console.WriteLine("Member name: {0}", e.TargetSite);
+                Console.WriteLine("Class defining member: {0}", e.TargetSite.DeclaringType);
+                Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);
+                Console.WriteLine("Message: {0}", e.Message);
+                Console.WriteLine("Method: {0}", e.Source);
+                Console.WriteLine("Stack: {0}", e.StackTrace);
+            }
+            
+            // The error has been handled, processing continues with the next statement.
+            Console.WriteLine("\n ***** Out of exception logic *****");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ III OOP with C#/7 Understanding Structured Exception Handling/CustomException/CustomException/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_SimpleException
+{
+    class Car
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+
+        #region Properties and fields.
+
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+        private bool carIsDead;
+
+        // Car has-a radio.
+        private Radio theMusicBox = new Radio();
+        #endregion
+
+        #region Constructors
+
+        public Car(){}
+
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        #endregion
+
+        #region Methods
+        public void CrankTunes(bool state)
+        {
+            //Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+
+        // See if the car has overheated.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine($"{PetName} is out of order...");
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine($"{PetName} has overheated");
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                    throw new Exception($"{PetName} has overheated!");
+                }
+                else
+                {
+                    Console.WriteLine($"=> CurrentSpeed = {CurrentSpeed}");
+                }
+            }
+        }
+        #endregion
+
+    }
+}
