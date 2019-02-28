db7a71ca174db5a8cbc2045b40196c3a833f9b42
Aliaksandr_Sidarau <aliaksandr_sidarau@epam.com>
2019-02-28 17:28:31 +0300

    Interfaces chapter works.

--- III OOP with C#/8 Interfaces/InterfaceHierarchy/InterfaceHierarchy/Program.cs
+++ III OOP with C#/8 Interfaces/InterfaceHierarchy/InterfaceHierarchy/Program.cs
@@ -10,12 +10,24 @@ namespace InterfaceHierarchy
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("***** Simple interface hierarchy *****\n");
+            
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Call from object level!
+            BitmapImage myBitMap = new BitmapImage();
+            myBitMap.Draw();
+            myBitMap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitMap.DrawUpsideDown();
+
+            // Get IAdvancedDraw explicitly.
+            IAdvancedDraw iAdvDraw = myBitMap as IAdvancedDraw;
+            if (iAdvDraw != null)
+            {
+                iAdvDraw.DrawUpsideDown();
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
