f793a431a04a27372be509f2ba34b7ab56d515aa
aliaksandr_sidarau <alexander.g.sidorov@gmail.com>
2019-03-13 23:01:38 +0300

    Started ClonablePoint

---
+++ III OOP with C#/8 Interfaces/ClonablePoint/ClonablePoint/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClonablePoint
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point (int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+
+        // Override Object.ToString()
+        public override string ToString() => $"X = {X}; Y = {Y}";
+    }
+}
