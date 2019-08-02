dca9b127fb4aa39d1ad616ed5cf5fdc9fa4c3e34
aliaksandr_sidarau <alexander.g.sidorov@gmail.com>
2019-08-03 00:07:36 +0300

    Lazy initialization example

---
+++ IV Advanced C# programming/13 Understanding Object lifetime/LAzyObjectInitialization/LAzyObjectInitialization/MediaPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAzyObjectInitialization
+{
+    // The MediaPlayer has an AllTracks object.
+    class MediaPlayer
+    {
+        // Assume these methods do something useful.
+        public void Play() { /*play a song */ }
+        public void Pause() { /*pause the song */ }
+        public void Stop() { /*stop playback*/ }
+        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>(() => 
+            {
+                Console.WriteLine("Creating AllTracks object!");
+                return new AllTracks();
+            } 
+        );
+        
+        public AllTracks GetAllTracks()
+        {
+            // Return all of the songs.
+            return allSongs.Value;
+        }
+
+    }
+}
---
+++ IV Advanced C# programming/13 Understanding Object lifetime/LAzyObjectInitialization/LAzyObjectInitialization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAzyObjectInitialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with Lazy Initialization! *****\n");
+
+            // This caller does not care about getting all songs,
+            // but indirectly created 10,000 objects!
+            MediaPlayer myPlayer = new MediaPlayer();
+            myPlayer.Play();
+            MediaPlayer yourPlayer = new MediaPlayer();
+            AllTracks yourMusic = yourPlayer.GetAllTracks();
+
+            Console.ReadLine();
+        }
+    }
+}
